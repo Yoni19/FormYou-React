0b2e95e974c12050b757f70894882c20c421dffb
Vivien-Ploix <vivien.ploix@gmail.com>
2020-12-03 15:12:55 +0100

    add connections admin formation, adminsession, adminuser delete

--- src/Pages/AdminUserGestion/index.jsx
+++ src/Pages/AdminUserGestion/index.jsx
@@ -7,58 +7,69 @@ import React, { useEffect, useState } from "react";
 
 const Session = () => {
   const [userslist, setUsersList] = useState([]);
-const fetchingUsers = () =>{
+  const tokenCookie = Cookies.get('token')
+  const fetchingUsers = () =>{
 
 
-    fetch("https://api-rails-form-you.herokuapp.com//users")
-      .then((response) => response.json())
-      .then((response) => {
-      setUsersList(response)
-          
+      fetch("https://api-rails-form-you.herokuapp.com//users")
+        .then((response) => response.json())
+        .then((response) => {
+        setUsersList(response)
         });
-    };
+  };
 
-    useEffect(() => {
-      fetchingUsers();
-    }, []);
-  ;
-console.log('la liste !:',userslist)
+  useEffect(() => {
+    fetchingUsers();
+  }, []);
+    
+  const handleClickDelete = (id) => {
+      fetch(`https://api-rails-form-you.herokuapp.com/users/${id}`, {
+        method: 'delete',
+        headers: {
+          'Authorization': `${tokenCookie}`, 
+          'Content-Type': 'application/json'
+        },
+      })
+      .then((response) => fetchingUsers())
+      .catch((error) => console.log(error))
+  }
 
-return (
-<div>
-<h1 className="text-center">Voici la liste des utilisateurs du site </h1>
-<Table striped bordered hover size="sm">
-  
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Email</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Status</th>
-      <th>validated</th>
-    </tr>
-  </thead>
-  <tbody>
- 
-  {userslist.map((usersList) => {
-      return  (
-  <tr>
-      <td>{usersList.id}</td>
-      <td>{usersList.email}</td>
-      <td>{usersList.firstname}</td>
-      <td>{usersList.lastname}</td>
-      <td>{usersList.status}</td>
-      <td>{usersList.validated === true? "True":"False"}</td>
-    </tr>)
-     })}
-   
-    </tbody>
+  return (
+    <div>
+    <h1 className="text-center">Voici la liste des utilisateurs du site </h1>
+    <Table striped bordered hover size="sm">
+      
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Status</th>
+          <th>Validated</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody> 
+        {userslist.map((user) => {
+            return  (
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.email}</td>
+                <td>{user.firstname}</td>
+                <td>{user.lastname}</td>
+                <td>{user.status}</td>
+                <td>{user.validated === true? "True":"False"}</td>
+                <td><Button className="btn btn-danger" onClick={() => handleClickDelete(user.id)}>Delete</Button></td>
+              </tr>
+            )
+        })}
+      </tbody>
 
 
-    </Table>
-</div>
-)
+      </Table>
+    </div>
+  )
 }
 
 export default Session
